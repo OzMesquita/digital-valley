c633afd0227a465776a0d4e2738e04be2cdb36c0
Luan Dharlin <luandharlinls@gmail.com>
2017-06-08 15:47:35 -0300

    ...

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,13 +1,16 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 
+@Entity
 public class Usuario {
 	
 	
-	//@Column(length=20, nullable=false, unique= true)
+	@Column(length=20, nullable=false, unique= true)
 	private String usuario;
 	
-	//@Column(length=20, nullable=false)
+	@Column(length=20, nullable=false)
 	private String senha;
 
 	public String getUsuario() {
