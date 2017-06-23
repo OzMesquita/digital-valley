7bfd70e470bd6447854a3cf98cb23bf658b412b4
Tágila Lima <tagila.lima96@gmail.com>
2017-06-23 17:22:54 -0300

    Inicio ProfessorDAO

---
+++ src/main/java/dao/ProfessorDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import model.Professor;
+
+public interface ProfessorDAO {
+
+	public void cadastrar(Professor professor);
+	
+	public Professor buscar(int id);
+	
+	public List<Professor> listar();
+}
--- src/main/java/model/Curso.java
+++ src/main/java/model/Curso.java
@@ -1,5 +1,45 @@
 package model;
 
-public class Curso {
+import java.util.ArrayList;
 
+public class Curso {
+	
+	private String codigo;
+	private String descricao;
+	private ArrayList<Disciplina> disciplinas;
+	
+	public Curso(String codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.disciplinas = new ArrayList<Disciplina>();
+	}
+	
+	public Curso() {
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public ArrayList<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(ArrayList<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	
+	
 }
