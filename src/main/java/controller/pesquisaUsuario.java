aa4f8ef6963d80ad78016069265d4fb5ac3bc96a
Luciano Lima <Luciano Lima>
2017-07-30 11:30:14 -0300

    tela adm e os controllers para ela

--- src/main/java/controller/pesquisaUsuario.java
+++ src/main/java/controller/pesquisaUsuario.java
@@ -6,7 +6,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Vector;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
