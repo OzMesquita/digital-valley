c33d793c20d78c2979bfac898d509141ed7181ac
Matheus Diógenes Andrade <matheusdiogenesandrade@gmail.com>
2017-09-28 16:16:20 -0300

    Removendo .env

--- src/main/java/util/Constantes.java
+++ src/main/java/util/Constantes.java
@@ -1,7 +1,5 @@
 package util;
 
-import com.harium.dotenv.Env;
-
 public class Constantes {
 
 	public final static String APP_URL;
@@ -13,17 +11,21 @@ public class Constantes {
 	public final static String APP_IMG_USER_DIR;
 	public final static Integer NUMBER_OF_ROWS_PER_PAGE;	
 	public final static String SESSION_MSG;
+	public final static String DATABASE_CONF_DIR;
+	
 	private Constantes() {
 		//
 	}
 
 	static {
-		APP_URL = Env.get("APP_URL");		
+		
+		DATABASE_CONF_DIR = "C:\\n2s\\bd.txt";
+		APP_URL = "/Controle_de_Acesso";
 		ADM_URL = APP_URL + "/view/adm";
 		APP_ASSETS_URL = APP_URL + "/assets2";
 		APP_JS_URL = APP_ASSETS_URL + "/js";
 		APP_IMG_URL = APP_ASSETS_URL + "/img";
-		APP_IMG_USER_DIR = APP_URL + "/img";
+		APP_IMG_USER_DIR = "C:\\n2s";
 		APP_CSS_URL = APP_ASSETS_URL + "/css";
 		NUMBER_OF_ROWS_PER_PAGE = 20;
 		SESSION_MSG = "msg";
